586118b46955bea4ba33ba67aa856aa9038874f3
Ben Harter-Murphy <benhartermurphy@gmail.com>
2021-02-23 19:22:04 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -2,12 +2,20 @@ console.log('%c HI', 'color: firebrick')
 
 const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
 const breedUrl = 'https://dog.ceo/api/breeds/list/all';
+const ulArea = document.getElementById("ul");
+let dropDown = document.getElementById('breed-dropdown');
+console.log("hi")
+let breeds = [];
+
+
+// dropDown.addEventListener("onchange", handleDropdown);
 
 function fetchImage(){
     return fetch(imgUrl)
     .then(response => response.json())
     .then(data => {
         data.message.forEach(image => renderImage(image));
+        
     });
 }
 
@@ -15,7 +23,10 @@ function fetchBreed() {
     return fetch(breedUrl)
     .then(response => response.json())
     .then(data => {
-        data.message.forEach(breed => renderBreed(breed));
+        // data.message.forEach(breed => renderBreed(breed));
+         breeds = Object.keys(data.message)
+         breeds.forEach(breed => renderBreed(breed))
+         
     });
 }
 
@@ -30,13 +41,27 @@ function renderImage(images){
 function renderBreed(breeds) {
     const ul = document.getElementById("dog-breeds")
     const li = document.createElement('li')
+    li.setAttribute('id', 'li')
+    li.addEventListener("click", handleClickEvent)
     li.innerHTML = breeds
     ul.appendChild(li)
+   
 };
+
+function handleClickEvent(e) {
+    e.target.style.color = "red";
+
+}
+
+// dropDown.addEventListener("onchange", handleDropDown)
+
+function handleDropDown(e) {
+    
+}
 
 document.addEventListener("DOMContentLoaded", function () {
     
     fetchImage();
     fetchBreed();
-    
+    dropDown = document.getElementById('breed-dropdown');
 })

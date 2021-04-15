40a340813049daca54748a31020e5f1c67658a03
Ben Harter-Murphy <benhartermurphy@gmail.com>
2021-04-15 14:36:18 -0400

    commit tree 2a37a0988a8f0b6a54cbad748783fd8cd2ff5465

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
 
 function fetchImage(){
-    return fetch(imgUrl)
-    .then(res =>  res.json())
-    .then(data => console.log(data));
+    return fetch(imgUrl).then((res) =>  res.json())
+    .then((json) => json.message.forEach(image => renderImage(image)));
+
 }
+
+function renderImage(image) {
+    const img = document.createElement('img');
+    img.src = image; 
+    const imgContainer = document.querySelector('#dog-image-container');
+    imgContainer.append(img);
+}
+
+
+
 
 
 
